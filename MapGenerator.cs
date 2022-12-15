e7fa03aad0526fcc937b025f7cbb45cb7c65aa9c
LYNX1239038120 <51261630+Yuyichn01@users.noreply.github.com>
2022-12-15 18:11:53 -0500

    Update MapGenerator.cs

--- MapGenerator.cs
+++ MapGenerator.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player section")]
+    public Transform player;
+
+    public int PlayerSpawnDistance;
+
+    [Header("Background section")]
+    public int StartingSpawns;
+
+    public Transform StartPrefabBackground;
+
+    public List<Transform> levelPartList;
+
+    private Vector3 lastEndPosition;
+
+    [Header("Ground section")]
+    public Transform StartPrefabGround;
+
+    public void Awake()
     {
-        
+        // generate background
+        lastEndPosition = StartPrefabBackground.Find("EndPosition").position;
+
+        for (int i = 0; i < StartingSpawns; i++)
+        {
+            Spawn();
+        }
+        DontDestroyOnLoad(transform.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // generate background
+        if (
+            Vector3.Distance(player.position, lastEndPosition) <
+            PlayerSpawnDistance
+        )
+        {
+            Spawn();
+        }
+    }
+
+    // Start is called before the first frame update
+    public void Spawn()
+    {
+        Transform chosenLevelPart =
+            levelPartList[Random.Range(0, levelPartList.Count)];
+        Transform lastLevelPartTransform =
+            Spawn(chosenLevelPart, lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform Spawn(Transform levelPart, Vector3 spawnPosition)
+    {
+        Transform levelPartTransform =
+            Instantiate(levelPart, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
 }
